32d627b1412a5532e8a57eedbdbafc89823217e8
r-kls <93781769+r-kls@users.noreply.github.com>
2021-12-17 23:57:49 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace PocetZnaku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] selected_array;
+            if (args.Length > 0) // Pokud je zadán argument a je platný, nastaví se nové pole.
+            {
+               selected_array = Arrays(args[0]);
+            }
+            else // Pokud není před spuštěním zadán argument, zvolí se malá písmena.
+            {
+                selected_array = Arrays("-a");
+            }
+            
+            bool work = true;
+            while (work) // Cyklus se opakuje dokud uživatel neopustí program (při metodě Exit nezvolí možnost a nebo ano).
+            {
+                Init(selected_array);
+                work = Exit();
+            }
+        }
+
+        static void Init(string [] selected_array) // Funkce která vypisuje uživateli všechny spočítané písmena v abecedním pořadí využitím funkce Count (jádro aplikace).
+        {
+            Console.WriteLine("Zadejte větu:");
+            string znaky = Console.ReadLine();
+
+            int pocet_pismen = 0;
+
+            for (int i = 0; i < selected_array.Length; i++) // Pro každý jeden cyklus se vybere další písmeno z pole selected_array, do proměnné pocet_pismen se uloží počet výskytů aktuálně počítaného písmene a pokud je vyšší než 0, vypíše počet výskytů daného písmene polečně se samostatným znakem.
+            {
+                pocet_pismen = Count(znaky, selected_array[i]);
+
+                if (pocet_pismen != 0)
+                {
+                    Console.WriteLine("{0} - {1}", selected_array[i], pocet_pismen);
+                    pocet_pismen = 0;
+                }
+            }
+        }
+
+        static int Count(string input_string, string input_character) // Funkce spočítá počet výskytů zadaného znaku (input_character) ze zadaného řetězce (input_string).
+        {
+            int occurrences = 0;
+
+            for (int i = 0; i < input_string.Length; i++)
+            {
+                string input_string_single = input_string[i].ToString();
+
+                if (input_string_single.Contains(input_character))
+                {
+                    occurrences++;
+                }
+            }
+
+            return occurrences;
+        }
+        static bool Exit() // Funkce se ukončí nebo pokračuje při rozhodnutí uživatele.
+        {
+            bool work = true;
+            bool exit_invalid = false;
+            do
+            {
+                Console.WriteLine("Přejete si odejít?\nStiskněte 'a' pro ukončení aplikace nebo 'n' pro zadání nové věty");
+                string volba = Console.ReadLine();
+                volba.ToLower();
+                switch (volba)
+                {
+                    case "n":
+                    case "ne":
+                        exit_invalid = false;
+                        break;
+
+                    case "a":
+                    case "ano":
+                        exit_invalid = false;
+                        work = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Neplatná volba");
+                        exit_invalid = true;
+                        break;
+                }
+            } while (exit_invalid);
+            return work;
+        }
+
+        static string[] Arrays(string arr_choice) // Funkce vrátí zvolené pole pomocí argumentu při spuštění aplikace.
+        {
+            string[] lower_alpha = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+            string[] upper_alpha = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            string[] alpha = new string[lower_alpha.Length + upper_alpha.Length]; // Tento řádek a další dva pod ním tvoří pole alpha což je lower_alpha a upper_alpha dohromady.
+            lower_alpha.CopyTo(alpha, 0);
+            upper_alpha.CopyTo(alpha, alpha.Length - upper_alpha.Length);
+            string[] num = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            switch (arr_choice)
+            {
+                case "-l":
+                case "-L":
+                    return lower_alpha;
+                    break;
+                case "-u":
+                case "-U":
+                    return upper_alpha;
+                    break;
+                case "-n":
+                case "-N":
+                    return num;
+                    break;
+                default:
+                    return lower_alpha;
+                    break;
+            }
+        }
+    }
+}
